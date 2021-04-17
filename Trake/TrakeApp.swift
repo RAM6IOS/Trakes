64439c4b6269a6892a2759c454ecbe5aea9d2b78
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2021-04-17 22:00:13 +0100

    Trake

--- Trake/TrakeApp.swift
+++ Trake/TrakeApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct TrakeApp: App {
     var body: some Scene {
         WindowGroup {
+            NavigationView{
             ContentView()
+            }
         }
     }
 }
