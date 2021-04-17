d90d25ebdb9be386d02e3fa3835149a8908f2bcf
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2021-04-17 15:53:30 +0100

    Initial Commit

---
+++ Trake/TrakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrakeApp.swift
+//  Trake
+//
+//  Created by MAC on 17/4/2021.
+//
+
+import SwiftUI
+
+@main
+struct TrakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
